3c291a7a8e4f5e11108895091e3a54edb037363e
hechengjun <hechengjun@yunxi-tec.com>
2019-09-11 09:33:10 +0800

    asdfa

---
+++ first/src/com/yunxi/day23/Javabean.java
@@ -0,0 +1,12 @@
+package com.yunxi.day23;
+/**
+ * javabean标准的定义类规范
+ * 1.私有化成员变量
+ * 2.一定要提供一个无参构造器
+ * 3.提供成员变量的set、get方法
+ *          4. 每个类单独成文件
+ *可利用这样的规范完成普适性操作。
+ *
+ * */
+public class Javabean {
+}
---
+++ first/src/com/yunxi/day23/SqlQuery.java
@@ -0,0 +1,7 @@
+package com.yunxi.day23;
+
+public class SqlQuery {
+    public <T> List<T> query(String sql,Object[]parameters,Class<T> cls){
+
+    }
+}
